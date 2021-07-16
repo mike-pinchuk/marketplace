db91cacfc8db047feaacda542f651d621c8e4707
Mykhailo Pinchuk <m.pinchuk@mobidev.biz>
2021-07-16 16:01:27 +0300

    feat(helmet): added app protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { expressMiddleware } from 'cls-rtracer';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
   app.use(expressMiddleware())
   await app.listen(3000);
 }
 bootstrap();
+
+
+
